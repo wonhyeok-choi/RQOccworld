ee7707a608e171f124290457de48f0ad4554fabc
wonhyeok-choi <smu06117@gmail.com>
2025-06-13 13:49:14 +0900

    remove guidetransformer

--- model/__init__.py
+++ model/__init__.py
@@ -6,7 +6,7 @@ from .VAE.residual_quantizer_v3 import ResidualVectorQuantizerV3
 
 from .transformer.pose_decoder import PoseDecoder
 from .transformer.pose_encoder import PoseEncoder
-from .transformer.PlanUtransformer import PlanUAutoRegTransformer, PlanUAutoRegTransformerGuide, PlanUAutoRegTransformerResidual
+from .transformer.PlanUtransformer import PlanUAutoRegTransformer, PlanUAutoRegTransformerResidual
 
 from .TransRQVAE import TransRQVAE
 from .TransRQVAE_rqvaealign import TransRQVAEalign
