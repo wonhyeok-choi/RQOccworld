0b87f8d14e81d8b881c4f11f7d3194735333c65f
wonhyeok-choi <smu06117@gmail.com>
2025-06-18 16:32:32 +0900

    current setup

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2,3 python eval_metric_stp3.py --py-config config/eval_cascadeworld_ms_3_rqvaealign.py --work-dir out/eval_poseguide --resume-from out/posguided/epoch_21.pth
+CUDA_VISIBLE_DEVICES=2,3 python eval_metric_stp3.py --py-config config/eval_cascadeworld_ms_3_rqvaealign.py --work-dir out/eval_poseguide --resume-from out/bestsetup_temp5/epoch_28.pth
